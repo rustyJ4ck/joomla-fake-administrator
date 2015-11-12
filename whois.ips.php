044c65de557d785a313b209e740a55de75c17104
j4ck <rustyj4ck@gmail.com>
2015-11-12 10:00:16 +0300

    abuse@mails

--- whois.ips.php
+++ whois.ips.php
@@ -1,10 +1,10 @@
 #!/usr/local/bin/php -n
 <?php
 /**
+ * @project: ondtp
  * @author: Golovkin Vladimir <rustyj4ck@gmail.com>
+ * @created: 11.11.2015 9:33
  */
-
-$myIps = ['178.215.110.80', '195.208.58.114'];
 
 $data = file_get_contents('log.txt');
 
@@ -12,11 +12,12 @@ preg_match_all('@^\d+\.\d+\.\d+\.\d+@m', $data, $matches);
 
 $ips = (array_unique($matches[0]));
 
-$ips0 = array_diff($ips, $myIps);
-
 $ipfw = 'whois "%s"' . PHP_EOL;
 
 foreach ($ips0 as $ip) {
-    echo $ip, PHP_EOL;
-    system(sprintf($ipfw, $ip));
+    exec(sprintf($ipfw, $ip), $buffer);
+    $buffer = join("\n", $buffer);
+    //Abuse contact for '46.0.0.0 - 46.0.31.255' is 'abuse@domru.ru'
+    preg_match('@Abuse contact for .* is \'(?P<abuse>.*)\'@U', $buffer, $matches);
+    echo $ip, ' ', @$matches['abuse'], PHP_EOL;
 }
